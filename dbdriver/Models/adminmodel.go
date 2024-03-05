a053fd537aaf12e40c238b3dc36aefb6b777f9be
SakthiMahendran <kingsakthi2005@gmail.com>
2024-03-05 19:39:41 +0530

    fromating

--- dbdriver/Models/adminmodel.go
+++ dbdriver/Models/adminmodel.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Admin struct {
-    Timestamp   time.Time `bson:"timestamp"`
-    Name        string    `bson:"admin_name"`
-    ID          string    `bson:"admin_id"`
-    Password    string    `bson:"admin_password"`
+	Timestamp time.Time `bson:"timestamp"`
+	Name      string    `bson:"admin_name"`
+	ID        string    `bson:"admin_id"`
+	Password  string    `bson:"admin_password"`
 }
