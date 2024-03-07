c9c63b22a000e70706fcd64d11b3770b2b1f0134
Renuga-Suresh03 <renugasuresh03@gmail.com>
2024-03-07 09:27:38 +0530

    admin controllers added

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"github.com/SakthiMahendran/SmartLibrary/dbdriver"
-	"github.com/SakthiMahendran/SmartLibrary/server"
-	"github.com/SakthiMahendran/SmartLibrary/server/controller"
+	"context"
+	"fmt"
+
+	"github.com/SakthiMahendran/SmartLibrary/dbdriver/controllers"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	dbdriver.Nothing()
-	controller.Nothing()
+	/*dbdriver.Nothing()
+	// controller.Nothing()
 
-	smartServer := server.NewServer()
-	smartServer.Start("8080")
+	// smartServer := server.NewServer()
+	// smartServer.Start("8080")*/
+
+	client, _ := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	adminController := controllers.NewAdminController(client, "SMLS", "Admin")
+	fmt.Printf("adminController.Auth(\"Abba\", \"uK1'Ra<5U'7W1n,\"): %v\n", adminController.Auth("Abba", "uK1'Ra<5U'7W1n,"))
 }
